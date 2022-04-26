3bdb5b63aea0093c6e64f2761637ae8df434736f
Anjana Wickrmasinghe <87361233+AnjaNawo@users.noreply.github.com>
2022-04-26 19:33:25 +0530

    Add files via upload

--- src/main/java/eg/power/crud/crud/resource/crudresource.java
+++ src/main/java/eg/power/crud/crud/resource/crudresource.java
@@ -1,5 +1,38 @@
 package eg.power.crud.crud.resource;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import eg.power.crud.crud.model.crudmodel;
+import eg.power.crud.crud.service.crudservice;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/crud")
 public class crudresource {
+	
+	
+	crudservice service = new crudservice();
+	
+	@Path("/insertion")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public crudmodel addPowercut(crudmodel powercut) {
+		return service.addPowercut(powercut);
+	
+	}
+	
+	@Path("/retrieve")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<crudmodel> getPowercut() throws SQLException {
+		return service.getPowercut();
+	
+	}
 
 }
+ 
